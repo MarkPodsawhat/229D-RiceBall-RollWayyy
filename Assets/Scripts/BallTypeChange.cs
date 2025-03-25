952b77ad6c7423ef26a9707e06ca10b2fedf604e
MicroVoxel <machenga.koko@gmail.com>
2025-03-26 04:16:20 +0700

    NewPlayerAndMatChangeScript

---
+++ Assets/Scripts/BallTypeChange.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BallTypeChange : MonoBehaviour
+{
+    public int matNo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player player = other.gameObject.GetComponent<Player>();
+            player.ChangeBallType(matNo);
+            Debug.Log("Change!!--1");
+        }
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,27 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     Rigidbody rb;
+    Collider collider;
+    Material newMaterial;
+    MeshRenderer meshRenderer;
+
     public GameObject face;
     public Camera cam;
+    public GameObject camfollower;
 
+    [Header("Speed")]
     public float speed = 5.0f;
 
+    [Header("Camera")]
     public float camX = 0;
     public float camY = 0;
     public float camZ = 0;
+
+    [Header("Material")]
+    public Material[] materials;
+
+    [Header("PhysicsMaterial")]
+    public PhysicsMaterial[] physicMaterial;
 
     private InputAction moveAction;
     private InputAction breakAction;
@@ -19,6 +32,9 @@ public class Player : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        meshRenderer = GetComponent<MeshRenderer>();
+
         moveAction = InputSystem.actions.FindAction("Move");
         breakAction = InputSystem.actions.FindAction("Break");
     }
@@ -42,11 +58,50 @@ public class Player : MonoBehaviour
             rb.angularVelocity = Vector3.zero;
         }
 
-       
     }
 
     private void LateUpdate()
     {
-        cam.transform.position = transform.position + new Vector3(camX, camY, camZ);
+        cam.transform.position = camfollower.transform.position + new Vector3(camX, camY, camZ);
+        cam.transform.LookAt(camfollower.transform.position);
+
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            camY += 0.5f;
+            camZ -= 1.0f;
+            if (camY > 3f)
+            {
+                camY = 3f;
+            }
+            if (camZ < -6f)
+            {
+                camZ = -6f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            camY -= 0.5f;
+            camZ += 1.0f;
+
+            if (camY < 1.5f)
+            {
+                camY = 1.5f;
+            }
+            if (camZ > -3f)
+            {
+                camZ = -3f;
+            }
+        }
     }
+
+    public void ChangeBallType(int matNo)
+    {   
+        if (matNo < materials.Length)
+        {
+            newMaterial = materials[matNo];
+            meshRenderer.sharedMaterial = newMaterial;
+        }
+    }
+
 }
